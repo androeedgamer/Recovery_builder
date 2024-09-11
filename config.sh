a7d3f761296a1a42425c0eadb3f846f33de290c5
androeedgamer <ravishankar80611@gmail.com>
2024-09-12 05:26:15 +0530

    config.sh

--- config.sh
+++ config.sh
@@ -3,10 +3,10 @@
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
 export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export BRANCH="twrp-7.1" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
+export DEVICE="elitexlte" # codename used in device tree
 export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
 export DT_BRANCH="twrp" # device tree branch
 export VENDOR="samsung" # device manufacturer or vendor
